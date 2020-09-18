3b8e715eb987aa11924242d2d5f17f2f5b2822ab
arthurnog <artuts874@gmail.com>
2020-09-18 10:03:15 -0300

    add func simple_get_data (sinto que eu não estou entendendo nada)

--- simple/simple.c
+++ simple/simple.c
@@ -52,3 +52,28 @@ typedef struct user_data_struct {
     char data[256];
 } user_data_struct;
 
+void *simple_constructor(godot_object *p_instance, void *p_method_data) {
+    user_data_struct *user_data = api->godot_alloc(sizeof(user_data_struct));
+    strcpy(user_data->data, "World from GDNative!");
+
+    return user_data;
+}
+
+void simple_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data) {
+    api->godot_free(p_user_data);
+}
+
+godot_variant simple_get_data(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
+    godot_string data;
+    godot_variant ret;
+    user_data_struct *user_data = (user_data_struct *)p_user_data;
+
+    api->godot_string_new(&data);
+    api->godot_string_parse_utf8(&data, user_data->data);
+    api->godot_variant_new_string(&ret, &data);
+    api->godot_string_destroy(&data);
+
+    return ret;
+}
+
+//parei em get_data
